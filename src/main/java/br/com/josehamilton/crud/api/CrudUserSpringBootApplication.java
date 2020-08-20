fb76113353bd8ee38a445918a071d01ec135bf0c
tonto <josehamiltonmartinsleite@gmail.com>
2020-08-20 13:45:34 -0300

    First Commit

---
+++ src/main/java/br/com/josehamilton/crud/api/CrudUserSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.josehamilton.crud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUserSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUserSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/josehamilton/crud/api/CrudUserSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.josehamilton.crud.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUserSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
